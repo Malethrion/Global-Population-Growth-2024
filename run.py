a3c37f308da3ca7aefdd1332c565fa056730ae02
Jin Nordén <rvphogir@students.codeinstitute.net>
2024-08-27 11:39:15 +0000

    Implement data fetching and processing for population data

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
+# Define the scope and credentials for Google Sheets API
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,8 +14,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('global_population_growth_2024')
 
-statistics = SHEET.worksheet('statistics')
 
-data = statistics.get_all_values()
+def get_population_data():
+    """
+    Retrieves the population data from the 'statistics' worksheet.
+    """
+    print("Fetching population data...\n")
+    statistics = SHEET.worksheet('statistics')
+    data = statistics.get_all_values()
 
-print(data)
+    # Debugging print to check headers
+    headers = data[0]
+    print("Headers:", headers)
+
+    # Convert to a list of dictionaries
+    data_dict = [dict(zip(headers, row)) for row in data[1:]]
+
+    # Convert 2023 and 2024 population fields to integers
+    for entry in data_dict:
+        if 'Population 2023' in entry and 'Population 2024' in entry:
+            entry['Population 2023'] = int(entry['Population 2023'])
+            entry['Population 2024'] = int(entry['Population 2024'])
+        else:
+            print(f"Missing population data in entry: {entry}")
+
+    return data_dict
